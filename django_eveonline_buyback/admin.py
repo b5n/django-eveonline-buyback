e4b5c3b1fa8b344c030656c51bb554345732fb80
b <b@bnunez.com>
2020-09-03 15:03:29 -0700

    ISSUE-4 Allow for parsing and appraisal of in game contract format
    - Added check for in game contract format
    - Added logic for parsing in game contract format
    - Added basic logging and exception handling
    - Added explicit exception for improperly sorted blue loot
    - Added error element to template

--- django_eveonline_buyback/admin.py
+++ django_eveonline_buyback/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from django_eveonline_buyback.models import BuybackSettings
 from django_singleton_admin.admin import DjangoSingletonModelAdmin
 
+
 admin.site.register(BuybackSettings, DjangoSingletonModelAdmin)
