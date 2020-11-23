3dec85620fa4da08e0b36f0092fea36742a44760
porowns <porowns@gmail.com>
2020-11-23 15:14:35 -0500

    Hook into package bindings, adapt templates to bootstrap 4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-eveonline-buyback',
-    version='0.0.2',
+    version=__import__('django_eveonline_buyback').__version__,
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
